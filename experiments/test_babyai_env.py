7e2669527176fdc62a3bfda2216b64aee63e0508
alexanderivanov2424 <alexander_ivanov@brown.edu>
2025-04-07 22:08:07 -0700

    babyai testing

--- experiments/test_babyai_env.py
+++ experiments/test_babyai_env.py
@@ -1,21 +1,22 @@
 import gym
-import babyai  
+import minigrid
+from minigrid.wrappers import *
+import babyai_text
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from agents.random_agent import RandomAgent
 
-from utils.run_utils_babyai import run_episode
+from utils.run_utils import run_episode
 from utils.experiment_data import ExperimentData
-from environment.babyai_env import create_babyai_env, get_available_babyai_envs
 
-# Choose test environment 
-env_name = "goto_red_ball"  
+env_name = "BabyAI-GoToRedBallGrey-v0"
+env_name = "BabyAI-MixedTrainLocal"
 experiment = ExperimentData(f"test_babyai_agent_{env_name}")
 
 # Create BabyAI environment
-env = create_babyai_env(env_name=env_name)
+env = gym.make(env_name)
 env.reset()
 
 # Select agent 
