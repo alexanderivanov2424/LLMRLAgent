96e41b111ea1dee269aef6c71e8273b60ed8d3f1
John Finberg <johnbfinberg@gmail.com>
2025-04-27 17:05:23 -0400

    feat: skyline DQN SLURM job

---
+++ jobs/skyline-dqn.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Request a GPU partition node and access to 1 GPU
+#SBATCH -p gpu --gres=gpu:1
+
+# Request 1 CPU core
+#SBATCH -n 4
+#SBATCH -t 24:00:00
+#SBATCH --mem=192G
+
+# Load a CUDA module
+module load cuda
+
+# Load a Python module
+module load python/3.11.0s-ixrhc3q
+
+# Activate the virtual environment
+source /users/jfinberg/scratch/LLMRLAgent/.venv/bin/activate
+
+# Move to the project directory
+cd /users/jfinberg/scratch/LLMRLAgent/experiments
+
+# Run program
+python run_hyperparameter_grid_search.py --agent dqn
