11e313243b1d3ccb8d8ac3e9aba3638fd5c11da8
alexanderivanov2424 <alexander_ivanov@brown.edu>
2025-05-01 20:09:44 -0700

    remove async calls

--- agents/llm_memory_agent.py
+++ agents/llm_memory_agent.py
@@ -6,9 +6,6 @@ from agents.base_agent import BaseAgent
 from environment.base_environment import Action, ActionResponse, MemoryResponse
 from agents.configs.base_config import LLMRLAgentConfig
 
-import asyncio
-from asyncio import TaskGroup
-
 class TerminateTaskGroup(Exception):
     """Exception raised to terminate a task group."""
 
@@ -101,11 +98,7 @@ class LLMMemoryAgent(BaseAgent):
 
     def _call_agent(self, prompt: str) -> ActionResponse:
         """Call the agent with the given prompt"""
-
-        response = None
-
-        async def async_chat():
-            response = chat(
+        response = chat(
                 messages=[
                     {
                         "role": "user",
@@ -115,17 +108,6 @@ class LLMMemoryAgent(BaseAgent):
                 model=self.model,
                 format=self.valid_response.model_json_schema(),
             )
-
-        async def run_with_timeout():
-            try:
-                async with TaskGroup() as group:
-                    group.create_task(async_chat)
-                    await asyncio.sleep(20)
-                    group.create_task(force_terminate_task_group())
-            except* TerminateTaskGroup:
-                return ActionResponse(action=0, reasoning="")
-
-        asyncio.run(run_with_timeout())
 
         if (
             response is None
